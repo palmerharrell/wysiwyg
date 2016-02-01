5661993dd51b1febbcf89544852de084b8c6c627
Palmer Harrell <palmerharrell@me.com>
2016-02-01 14:20:52 -0600

    Created famous people array

--- javascripts/wysiwyg.js
+++ javascripts/wysiwyg.js
@@ -1,6 +1,49 @@
 // Instructions
 
 // Create an array of objects that represents famous people (see structure below).
+var people = [
+	{
+	  title: "Comedian",
+	  name: "Terry Jones",
+	  bio: "British comedian, screenwriter, actor, film director, historian and author. He is best known as a member of the Monty Python comedy troupe.",
+	  image: "https://upload.wikimedia.org/wikipedia/commons/7/70/Terry_Jones.jpg"
+	  lifespan: {
+	    birth: 1942,
+	    death: ""
+	  }
+	},
+	{
+	  title: "Musician",
+	  name: "Rick James",
+	  bio: 'American musician and composer. Known as the "king of punk funk".',
+	  image: "https://upload.wikimedia.org/wikipedia/commons/4/4b/Rick_James_in_Lifestyles_of_the_Rich_1984.JPG"
+	  lifespan: {
+	    birth: 1948,
+	    death: 2004
+	  }
+	},
+	{
+	  title: "Actor / Martial Artist",
+	  name: "Brandon Lee",
+	  bio: 'American actor and martial artist. He was the son of martial arts film actor Bruce Lee.',
+	  image: "https://upload.wikimedia.org/wikipedia/en/a/a1/Brandon_Lee.jpg"
+	  lifespan: {
+	    birth: 1965,
+	    death: 1993
+	  }
+	},
+	{
+	  title: "Comedian",
+	  name: "Pauly Shore",
+	  bio: ' American actor, comedian, director, writer and producer who starred in several comedy films in the 1990s and hosted a video show on MTV in the late 1980s and early 1990s.',
+	  image: "https://upload.wikimedia.org/wikipedia/en/a/a1/Brandon_Lee.jpg"
+	  lifespan: {
+	    birth: 1968,
+	    death: ""
+	  }
+	}
+];
+
 // Create a text input in your DOM.
 // Beneath that, create a container, block element in your DOM.
 // Create a DOM element for each of the objects inside the container. Style your person elements 
@@ -16,22 +59,5 @@
 // When you press the enter/return key when typing in the input field, then the content of the input 
 // field should immediately be blank.
 
-// <!-- person element? what??? -->
-// <person>
-//   <header>Name and title go here</header>
-//   <section>Bio and image go here</section>
-//   <footer>Lifespan info goes here</footer>
-// </person>
 
-// ~~~ Object Structure ~~~
-// {
-//   title: "Samurai",
-//   name: "Tomoe Gozen",
-//   bio: "Serving under Minamoto Yoshinaka, Tomoe was one of his finest soldiers, and her skills in battle dwarfed many of those held by even the strongest men in her unit.",
-//   image: "https://upload.wikimedia.org/wikipedia/commons/4/48/Tomoe-Gozen.jpg"
-//   lifespan: {
-//     birth: 1747,
-//     death: 1797
-//   }
-// }
 
